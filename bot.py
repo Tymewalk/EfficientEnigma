9bfdde3f9546eae2408ed5e304dfa506bd29fbcb
Tymewalk <Tymewalk@users.noreply.github.com>
2017-07-01 21:43:22 -0500

    Add simple skeleton log-in bot Currently can only read the token and log in. Does nothing else, but it's a start.

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord, asyncio, os
+
+client = discord.Client()
+
+f = open("{}/{}".format(os.path.dirname(os.path.realpath(__file__)), "token"))
+# Discord tokens have no whitespace, remove any that might've slipped in there
+bot_token = f.read().rstrip().lstrip()
+f.close()
+
+@client.event
+async def on_ready():
+    print('Successfully logged in as {} (ID {}).'.format(client.user.name, client.user.id))
+
+try:
+    # If any background tasks need to run, start them here
+    client.run(bot_token)
+except KeyboardInterrupt:
+    # Properly exit if we get a Ctrl-C
+    client.logout()
+finally:
+    # If anything needs to save, do it here
+    print("Done running.")

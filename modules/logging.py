1ba1542eece280f0c7e547b1256a65ea00d35dc8
Tymewalk <Tymewalk@users.noreply.github.com>
2017-08-09 16:47:44 -0500

    Prevent embeds from causing problems Apparently it counts as an edit when an embed loads. Weird, but whatever.

--- modules/logging.py
+++ modules/logging.py
@@ -5,7 +5,8 @@ import discord.utils, asyncio
 log_channel = "(set this)"
 
 async def log_message_edit(client, old, new):
-	await client.send_message(discord.utils.get(old.server.channels, name=log_channel, type=discord.ChannelType.text), "User {} edited their message in {}:\nOld: {}\nNew: {}".format(str(old.author), str(old.channel), old.content, new.content))
+	if not old.content == new.content:
+		await client.send_message(discord.utils.get(old.server.channels, name=log_channel, type=discord.ChannelType.text), "User {} edited their message in {}:\nOld: {}\nNew: {}".format(str(old.author), str(old.channel), old.content, new.content))
 
 async def log_message_delete(client, message):
 	await client.send_message(discord.utils.get(message.server.channels, name=log_channel, type=discord.ChannelType.text), "User {} deleted their message in {}:\n{}\n".format(str(message.author), str(message.channel), message.content))

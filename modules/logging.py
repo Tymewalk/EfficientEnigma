fcf21d8ac3354d36e3f8fc201c4b7a3c2fb347b1
Tymewalk <Tymewalk@users.noreply.github.com>
2017-09-09 11:13:58 -0500

    Partially fix #7 by specifying a channel as an argument It might be possible to use any channel, but this is just as good for now.

--- modules/logging.py
+++ modules/logging.py
@@ -10,13 +10,13 @@ f.close()
 log_channel = settings["log_channel"]
 
 async def log_message_edit(client, old, new):
-	if not old.content == new.content:
-		await client.send_message(discord.utils.get(old.server.channels, name=log_channel, type=discord.ChannelType.text), "User {} edited their message in {}:\nOld: {}\nNew: {}".format(str(old.author), str(old.channel), old.content, new.content))
+    if not old.content == new.content:
+        await client.send_message(discord.utils.get(old.server.channels, name=log_channel, type=discord.ChannelType.text), "User {} edited their message in {}:\nOld: {}\nNew: {}".format(str(old.author), str(old.channel), old.content, new.content))
 
 async def log_message_delete(client, message):
-	await client.send_message(discord.utils.get(message.server.channels, name=log_channel, type=discord.ChannelType.text), "User {} deleted their message in {}:\n{}\n".format(str(message.author), str(message.channel), message.content))
+    await client.send_message(discord.utils.get(message.server.channels, name=log_channel, type=discord.ChannelType.text), "User {} deleted their message in {}:\n{}\n".format(str(message.author), str(message.channel), message.content))
 
 # Now setup
 def setup_hooks(hooktable):
-	hooktable["edit"].append(log_message_edit)
-	hooktable["delete"].append(log_message_delete)
+    hooktable["edit"].append(log_message_edit)
+    hooktable["delete"].append(log_message_delete)
--- modules/nostalgia.py
+++ modules/nostalgia.py
@@ -6,30 +6,35 @@ from datetime import *
 
 titles = ["teacher", "scientist", "scholar", "leader", "fighter", "mage", "wizard", "noble", "swordsman", "rifleman", "archer"]
 async def nostalgia(message, client):
+    searchin = False
+    if re.findall("<#[0-9]+>", message.content):
+        searchin = message.server.get_channel(re.sub("[\<\#\>]", "", re.findall("<#[0-9]+>", message.content)[0]))
+    else:
+        searchin = message.channel
     messagelist = list()
     randomtime = datetime.utcfromtimestamp(random.randint(calendar.timegm(message.channel.created_at.timetuple()), calendar.timegm(datetime.now().timetuple())))
-    async for message in client.logs_from(message.channel, limit=3, around=randomtime):
-        messagelist.append(message)
+    async for scan in client.logs_from(searchin, limit=3, around=randomtime):
+        messagelist.append(scan)
     rand_message = random.choice(messagelist)
     output = rand_message.content
     # Filter user pings
     for m in re.findall("<@[0-9]+>", output):
         userid = re.sub("[\<\@\>]", "", m)
-        output = re.sub(m, "{}".format(str(message.server.get_member(userid))[:-5]), output)
+        output = re.sub(m, "\@{}".format(str(message.server.get_member(userid))[:-5]), output)
     # Filter another type of user pings
     for m in re.findall("<@![0-9]+>", output):
         userid = re.sub("[\<\@\!\>]", "", m)
-        output = re.sub(m, "{}".format(str(message.server.get_member(userid))[:-5]), output)
+        output = re.sub(m, "\@{}".format(str(message.server.get_member(userid))[:-5]), output)
     # Filter role pings
     for m in re.findall("<@&[0-9]+>", output):
         roleid = re.sub("[\<\@\&\>]", "", m)
         output = re.sub(m, "\@{}".format(discord.utils.get(message.server.roles, id=roleid).name), output)
     # Filter everyone pings
     for m in re.findall("@everyone", output):
-        output = re.sub(m, "(everyone)", output)
+        output = re.sub(m, "\@everyone)", output)
     # Filter here pings
     for m in re.findall("@here", output):
-        output = re.sub(m, "(here)", output)
+        output = re.sub(m, "\@here)", output)
     await client.send_message(message.channel, "The great {} \"{}\" once said:\n\n{}".format(random.choice(titles), rand_message.author.name, output))
 
 # Add the commands to the global command table.

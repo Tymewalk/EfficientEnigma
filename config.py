6fe0de035af1eff44456c4257728bb934708912e
Tymewalk <Tymewalk@users.noreply.github.com>
2018-03-06 17:57:06 -0600

    Don't destroy old settings when updating the token Very annoying if several servers already have settings... that would then have to be completely redone

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import json, os
 
-settings = dict()
 
 token = input("Enter your bot's token: ")
-settings["token"] = token
 
+# Check if there are already settings - if not, just initialize them
+if os.path.isfile("{}/{}".format(os.path.dirname(os.path.realpath(__file__)), "settings.json")):
+	f = open("{}/{}".format(os.path.dirname(os.path.realpath(__file__)), "settings.json"), 'r')
+	settings = json.load(f)
+	f.close()
+else:
+	settings = dict()
+
+settings["token"] = token
 f = open("{}/{}".format(os.path.dirname(os.path.realpath(__file__)), "settings.json"), 'w')
 json.dump(settings, f)
 f.close()

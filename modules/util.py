3c9fbb2a8b0f88acc94a476137da57048a978cc5
Alex <32816774+alex85120@users.noreply.github.com>
2018-06-15 16:58:57 -0400

    Update ping command
    Add an millisecond counter to fix issue #20

--- modules/util.py
+++ modules/util.py
@@ -1,7 +1,13 @@
+import aiohttp, time
+
 # util.py
 # Various utilities, such as !ping.
 async def ping(message, client):
-    await client.send_message(message.channel, "{} :ping_pong: Pong!".format(message.author.mention))
+    start = time.time()
+    async with aiosession.get('https://discordapp.com'):
+        duration = time.time() - start
+    duration = round(duration * 1000)
+    await client.send_message(message.channel, "{} :ping_pong: Pong! (**{}ms**)".format(message.author.mention, duration))
 
 # Add the commands to the global command table.
 def setup_command_table(table, helptable):

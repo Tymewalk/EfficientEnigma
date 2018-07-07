78046e301bf78e99091423c8f6a88f2d1603e592
Tymewalk <Tymewalk@users.noreply.github.com>
2018-07-07 16:53:55 -0500

    Add the basics of the new help command

--- modules/help.py
+++ modules/help.py
@@ -1,17 +1,46 @@
 # help.py
 # Controls the !help command.
+import re
 
-async def help(message, client):
-	# TEMPORARY - WILL BE REMOVED SOON
-    await client.send_message(message.channel, """Here's a list of what I can do:
+# Specify some text up here
+pagetext = """Type !help (page number) to see that page's commands.
+1 - Generic commands
+2 - Roles"""
+
+generictext = """**Generic Commands**
 !nostalgia - Look at past messages from different channels, and relive the moment.
 !8ball - Ask a question, get an answer!
-!giverole - Gives you a role.
 !roll - Rolls dice. Submit in XdY.
-!listroles - Lists all the roles that can be given.
 !ping - Simple ping. That's all.
-!removerole - Removes a role from you.
-!about - Show information about the bot.""")
+!about - Show information about the bot.
+!help - List commands and get help on how to use the bot!"""
+
+roletext = """**Role Commands**
+If your server lets you self-assign roles, you'll be able to use these commands.
+
+!listroles - Lists all the roles that can be given.
+!giverole (role name) - Gives you a role.
+!removerole (role name) - Removes a role from you."""
+
+async def help(message, client):
+	# If the length of the message is more than 6 characters, someone specified a page they'd like
+    if len(message.content) > 6:
+    	page = re.sub("!help ", "", message.content)
+    else:
+    	# If it isn't, no page is requested
+    	page = False
+    
+    if not page:
+    	await client.send_message(message.author, pagetext)
+    elif page == "1":
+    	await client.send_message(message.author, generictext)
+    elif page == "2":
+    	await client.send_message(message.author, roletext)
+    else:
+    	await client.send_message(message.author, "Sorry, that's not a valid page number! Try !help to see a list of page numbers.")
+    
+    
+    
 
 # Add the commands to the global command table.
 def setup_command_table(table):

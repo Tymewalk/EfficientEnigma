11ebac89314e51cf19cff3f62a4d179874b32b54
Tymewalk <Tymewalk@users.noreply.github.com>
2020-04-05 20:29:44 -0500

    Actually check if we're supposed to be sending welcome/leave messages or not how did I miss this...?

--- modules/welcome.py
+++ modules/welcome.py
@@ -19,14 +19,16 @@ def format_welcome_message(member, message):
 async def announce_welcome(client, member):
 	load_settings()
 	server = member.server
-	welcome_channel = settings[server.id]["welcome_channel"]
-	await client.send_message(discord.utils.get(server.channels, name=welcome_channel, type=discord.ChannelType.text), format_welcome_message(member, settings[server.id]["welcome_message"]))
+	if settings[server.id]["use_welcome"]:
+		welcome_channel = settings[server.id]["welcome_channel"]
+		await client.send_message(discord.utils.get(server.channels, name=welcome_channel, type=discord.ChannelType.text), format_welcome_message(member, settings[server.id]["welcome_message"]))
 
 async def announce_leave(client, member):
 	load_settings()
 	server = member.server
-	welcome_channel = settings[server.id]["leave_channel"]
-	await client.send_message(discord.utils.get(server.channels, name=welcome_channel, type=discord.ChannelType.text), format_welcome_message(member, settings[server.id]["leave_message"]))
+	if settings[server.id]["use_leave"]:
+		welcome_channel = settings[server.id]["leave_channel"]
+		await client.send_message(discord.utils.get(server.channels, name=welcome_channel, type=discord.ChannelType.text), format_welcome_message(member, settings[server.id]["leave_message"]))
 
 def setup_hooks(hooktable):
     hooktable["member_join"].append(announce_welcome)
